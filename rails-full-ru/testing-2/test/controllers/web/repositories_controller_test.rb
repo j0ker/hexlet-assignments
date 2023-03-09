2d0b071a463cdef875e2cca14bed7b4a2490dd44
@hexlet/cli <support@hexlet.io>
2023-03-09 18:57:13 +0300

    submit rails-full-ru/testing-2

--- rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -10,7 +10,7 @@ class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
     stub_request(:get, 'https://api.github.com/repos/j0ker/first_app').
       with(headers: {
             'Accept'=>'application/vnd.github.v3+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Content-Type'=>'application/json', 'User-Agent'=>'Octokit Ruby Gem 5.6.1'
+            'Content-Type'=>'application/json', 'User-Agent'=>'Octokit Ruby Gem 4.25.1'
         }).to_return(status: 200, body: @response_body, headers: {})
   end
 
