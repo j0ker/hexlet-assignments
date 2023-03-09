25c66f8e59529b350945d26b58f49ed14f09ad99
@hexlet/cli <support@hexlet.io>
2023-03-09 15:58:00 +0300

    submit rails-full-ru/jbuilder

---
+++ rails-full-ru/jbuilder/app/serializers/user_serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :address, :full_name
+
+  has_many :posts
+
+  def full_name
+    [object.first_name, object.last_name].join(' ')
+  end
+end
